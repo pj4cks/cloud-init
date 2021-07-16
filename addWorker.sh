e69cea93845c7fbe0c727d6c6a98eeb722c0f5b6
Martino Jones <martino@martinojones.com>
2021-07-16 09:48:41 -0400

    Added scripts for bootstrapping.

--- addWorker.sh
+++ addWorker.sh
@@ -1,1 +1,8 @@
-kubeadm token create --print-join-command
+## RUN THIS ON THE MASTER NODE ##
+NODE_TOKEN=`sudo cat /var/lib/rancher/k3s/server/node-token`
+MASTER_IP=`ip addr | grep global | grep eth | grep -v 'flannel' | awk '{print $2}' | awk -F'/' '{print $1}'`
+echo "sudo curl -sfL https://get.k3s.io | K3S_URL=https://$MASTER_IP:6443 K3S_TOKEN=$NODE_TOKEN sh -"
+
+
+## THIS MUST RUN ON THE MASTER AFTER THE NODE IS ADDED ##
+kubectl label node <NODE NAME> node-role.kubernetes.io/worker=worker
--- initialBuild.sh
+++ initialBuild.sh
@@ -1,5 +1,4 @@
 
-
 # Install metallb
 kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.10.2/manifests/namespace.yaml
 kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.10.2/manifests/metallb.yaml
