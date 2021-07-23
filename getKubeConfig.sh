4e9eb2de6e62e579a51b8d1bb567a8296fda0ec3
Martino Jones <martino@martinojones.com>
2021-07-23 09:02:58 -0400

    Fixed get config.

--- getKubeConfig.sh
+++ getKubeConfig.sh
@@ -1,5 +1,5 @@
 ### THIS NEEDS TO BE RUN ON MASTER NODE ###
 MASTER_IP=`ip addr | grep global | grep eth | grep -v 'flannel' | awk '{print $2}' | awk -F'/' '{print $1}'`
-cat k3s.yaml | sed "s/127.0.0.1/$MASTER_IP/"
+cat /etc/rancher/k3s/k3s.yaml | sed "s/127.0.0.1/$MASTER_IP/"
 
 ### AFTER THIS IS RUN, PLACE ON YOUR COMPUTER ###
